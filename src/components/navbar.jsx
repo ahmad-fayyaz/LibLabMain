a1c7e1b4f85d096e5a748ee7f4cc6310455e5476
afayyaz1 <afayyaz1@swarthmore.edu>
2024-07-17 13:45:01 -0400

    responsive navbar for mobile

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,15 +2,8 @@ import logo from "../assets/logo.png"
 import { Mail } from 'lucide-react';
 import { Moon } from 'lucide-react';
 import { Music2 } from 'lucide-react';
-import {Menu, X } from 'lucide-react'; 
-import { useState } from "react";
 
 const Navbar = () => {
-    const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
-
-        const toggleNavbar = () => {
-            setMobileDrawerOpen(!mobileDrawerOpen);
-        };
   return (
     <nav className="sticky top-0 z-50 py-3 w-full">
       <div className="container px-2 mx-auto relative">
@@ -18,21 +11,16 @@ const Navbar = () => {
           <div className="flex items-center flex-shrink-0">
             <img className="h-12 w-12 mr-2" src={logo} alt="" />
           </div>
-          <ul className="hidden lg:flex space-x-5">
+          <ul className="flex space-x-5">
             <li><a className="font-bold underline" href="#">docs.</a></li>
             <li><a href="#"><Mail size={20} /></a></li>
             <li><a href="#"><Moon size={20} /></a></li>
             <li><a href="#"><Music2 size={20} /></a></li>
           </ul>
-          <div className="lg:hidden md:flex flex-col justify-end">
-            <button onClick={toggleNavbar}>
-                {mobileDrawerOpen ? <X /> : <Menu />}
-            </button>
-          </div>
         </div>
       </div>
     </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
