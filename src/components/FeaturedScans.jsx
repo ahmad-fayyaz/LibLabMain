8dfbf1923f22faf0626303d2ed9405f2fa01a400
afayyaz1 <afayyaz1@swarthmore.edu>
2024-07-20 20:04:14 -0400

    ...

--- src/components/FeaturedScans.jsx
+++ src/components/FeaturedScans.jsx
@@ -5,7 +5,7 @@ const FeaturedScans = () => {
         {
             title: 'LibLab Bear',
             description: 'Our first scan is a bear!',
-            iframeUrl: 'LibLab/bear.glb'
+            iframeUrl: '/bear.glb'
         },
         {
             title: 'Scan 2',
